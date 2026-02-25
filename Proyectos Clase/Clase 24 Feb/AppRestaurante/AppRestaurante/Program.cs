5c412d68946aa0e7008c3d8977bdc441135cff1b
27Henaoo <juanhenao1130324@correo.itm.edu.co>
2026-02-25 18:35:12 -0500

    Proyecto Restaurante Clases definidas correctamente

--- Proyectos Clase/Clase 24 Feb/AppRestaurante/AppRestaurante/Program.cs
+++ Proyectos Clase/Clase 24 Feb/AppRestaurante/AppRestaurante/Program.cs
@@ -1,2 +1,56 @@
 ﻿Console.WriteLine("Bienvenido Al Restaurante");
 
+public class Clientes
+{
+    public int Id { get; set; }
+    public string? Cedula { get; set; }
+    public string? Nombre { get; set; }
+    public string? Correo { get; set; }
+    public DateTime Fecha { get; set; }
+    public bool Activo { get; set; }
+
+    public List<Ventas>? Ventas { get; set; }
+}
+
+public class Productos
+{
+    public int Id { get; set; }
+    public string? Codigo { get; set; }
+    public string? Nombre { get; set; }
+    public decimal Cantidad { get; set; }
+    public decimal Valor { get; set; }
+
+    public List<Ventas_Productos>? Ventas_Productos { get; set; }
+}
+
+public class Ventas
+{
+    public int Id { get; set; }
+    public string? Codigo { get; set; }
+    public DateTime Fecha { get; set; }
+    public decimal Total{ get; set; }
+    public decimal Descuento { get; set; }
+
+    public int Cliente { get; set; }
+    public Clientes? _Cliente { get; set; }
+
+    public List<Ventas_Productos>? Ventas_Productos { get; set; }
+}
+
+
+
+public class Ventas_Productos
+{
+    public int Id { get; set; }
+
+    public decimal Cantidad { get; set; }
+    public decimal Valor { get; set; }
+    public decimal SubTotal{ get; set; }
+
+
+    public int Producto { get; set; }
+    public int Venta { get; set; }
+
+    public Productos? _Producto { get; set; }
+    public Ventas? _Venta{ get; set; }
+}
